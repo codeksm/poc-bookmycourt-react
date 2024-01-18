9499fbb8263d82be5d992b119b7bc44486f33fdf
Manjunath KS <manu.manjunath6316@gmail.com>
2024-01-18 20:57:10 +0530

    Add calendar to badminton page

--- src/components/Badminton.js
+++ src/components/Badminton.js
@@ -1,20 +1,23 @@
 import { Button } from "antd";
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { Outlet } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Scheduler from "./Scheduler";
 import BookingPanel from "./BookingPanel";
-import { Carousel } from "antd";
+import { Carousel, Calendar, Popover } from "antd";
 import "./Badminton.css";
-import { Tabs } from "antd";
+import dayjs from "dayjs";
+import { Tabs, DatePicker } from "antd";
+import { CalendarOutlined } from "@ant-design/icons";
 
 const { TabPane } = Tabs;
 
 const Badminton = () => {
   const navigate = useNavigate();
   const [selectedSlots, setSelectedSlots] = useState(new Set());
-  const tabData = [1, 2, 3, 4, 5, 6];
+  const [visible, setVisible] = useState(false);
+  const [selectedDate, setSelectedDate] = useState(dayjs());
+  const tabData = [1, 2, 3, 4, 5];
 
   useEffect(() => {
     // Update the document title using the browser API
@@ -25,6 +28,27 @@ const Badminton = () => {
     //setSelectedCard(card);
     navigate("/playground");
   };
+
+  const setToday = () => {
+    //setSelectedCard(card);
+    setSelectedDate(dayjs());
+    setVisible(false);
+  };
+
+  const handleDateClick = (value) => {
+    setSelectedDate(value);
+    console.log("Date " + value);
+    setVisible(false);
+  };
+
+  const content = (
+    <Calendar
+      className="calendar"
+      value={selectedDate}
+      fullscreen={false}
+      onChange={handleDateClick}
+    />
+  );
 
   return (
     <>
@@ -43,8 +67,29 @@ const Badminton = () => {
           </div>
         </Carousel>
       </div>
+
       <div className="badminton">
         <div className="badminton-courts">
+          <div className="badminton-courts-tab-header">
+            <Popover
+              content={content}
+              title={
+                selectedDate ? selectedDate.format("MMMM YYYY") : "Select Date"
+              }
+              trigger="click"
+              open={visible}
+              onOpenChange={setVisible}
+            >
+              <span className="date-month" onClick={() => setVisible(!visible)}>
+                {selectedDate
+                  ? selectedDate.format("DD MMMM YYYY")
+                  : "Select Date"}
+              </span>
+              <span className="date-today" onClick={() => setToday()}>
+                Today
+              </span>
+            </Popover>
+          </div>
           <Tabs defaultActiveKey="1" centered>
             {tabData.map((tab, index) => (
               <TabPane tab={`Court ${index + 1}`} key={index + 1}>
